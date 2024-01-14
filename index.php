31afaf90f7a51df23e9cd1b9cf2dc45120fa5f31
alielzahaby74 <alizahaby2000@gmail.com>
2024-01-14 11:19:45 +0200

    optimizing code

--- index.php
+++ index.php
@@ -14,22 +14,22 @@ $path = "/login/embed/" . urlencode($embedpath);
 $json_nonce = json_encode(md5(uniqid()));
 $json_current_time = json_encode(time());
 $json_session_length = json_encode(3600);
-$json_external_user_id = json_encode("test-id-123");
-$json_first_name = json_encode("Data");
-$json_last_name = json_encode("Team Cloudypedia");
-$json_permissions = json_encode( array ( "see_user_dashboards", "see_lookml_dashboards", "access_data", "see_looks" ) );
-$json_models = json_encode( array ( "future_x" ) );
-$json_group_ids = json_encode( array ( ) );  // just some example group ids
-$json_external_group_id = json_encode("");
-// $json_user_attributes = json_encode( array ( ) ) ?? "{}";  // just some example attributes
-$json_user_attributes = "{}";  // just some example attributes
-// NOTE: accessfilters must be present and be a json hash. If you don't need access filters then the php
-// way to make an empty json hash as an alternative to the below seems to be:
-// $accessfilters = new stdClass()
-$accessfilters = array ( "future_x" => array ( "Instituton" => 55830, "Year" => 2023 ));//default value is []
-$json_accessfilters = json_encode($accessfilters); //its still wrong but the problem now is with the signature
+$json_external_user_id = json_encode("test-id-123"); //create a unique id for each user
+$json_first_name = json_encode("Data"); //first name of the user
+$json_last_name = json_encode("Team Cloudypedia"); //last name of the user
+$json_permissions = json_encode([
+  //permissions for the user
+  "see_user_dashboards",
+  "see_lookml_dashboards",
+  "access_data",
+  "see_looks"
+]);
+$json_models = json_encode(array ("future_x"));
+$json_group_ids = json_encode( array ( ) );  //default value is []
+$json_external_group_id = json_encode(""); //default value is ""
+$json_user_attributes = json_encode( array(), JSON_FORCE_OBJECT);  // default value is {}
+$accessfilters = json_encode( array(), JSON_FORCE_OBJECT); //default value is {}
 
-var_dump($json_accessfilters);
 $stringtosign = "";
 $stringtosign .= $host . "\n";
 $stringtosign .= $path . "\n";
@@ -42,9 +42,12 @@ $stringtosign .= $json_models . "\n";
 $stringtosign .= $json_group_ids . "\n";
 $stringtosign .= $json_external_group_id . "\n";
 $stringtosign .= $json_user_attributes . "\n";
-$stringtosign .= $json_accessfilters;
+$stringtosign .= $accessfilters;
 
-$signature = trim(base64_encode(hash_hmac("sha1", utf8_encode($stringtosign), $secret, $raw_output = true)));
+$iso = $stringtosign;
+$stringtosign = mb_convert_encoding($iso, 'UTF-8', 'ISO-8859-1'); //just a utf-8 encoding but the php utf-8 function is deprecated
+
+$signature = trim(base64_encode(hash_hmac("sha1", $stringtosign, $secret, $raw_output = true)));
 
 $queryparams = array (
   // all fields are required and must be in the shown order, so don't sort them alphabetically or something
@@ -57,7 +60,7 @@ $queryparams = array (
     'group_ids' => $json_group_ids,
     'external_group_id' => $json_external_group_id, 
     'user_attributes' => $json_user_attributes,
-    'access_filters'  =>  $json_accessfilters,
+    'access_filters'  =>  $accessfilters,
     'first_name'  =>  $json_first_name,
     'last_name' =>  $json_last_name,
     'force_logout_login'  =>  false,
